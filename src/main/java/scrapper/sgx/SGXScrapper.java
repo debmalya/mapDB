3704b89f865d5f92ec704c32996adf865ebcb515
debmalyajash <debmalya.jash@gmail.com>
2016-07-14 18:59:15 +0800

    SG X

--- src/main/java/scrapper/sgx/SGXScrapper.java
+++ src/main/java/scrapper/sgx/SGXScrapper.java
@@ -15,14 +15,96 @@
  */
 package scrapper.sgx;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * @author debmalyajash
  *
  */
 public class SGXScrapper {
 
-}
+	/**
+	 * 
+	 */
+	private static final int PREFERRED_WIDTH = 35;
 
+	private static final String FOOL = "http://www.sgx.com";
+
+	private static final Logger LOGGER = Logger.getLogger(SGXScrapper.class);
+
+	public static void main(String... args) throws IOException {
+		fooled();
+	}
+
+	/**
+	 * @throws IOException
+	 * 
+	 */
+	private static void fooled() throws IOException {
+		Element doc = Jsoup.connect(FOOL).get();
+		if (LOGGER.isDebugEnabled()) {
+			debugLog(doc.text(), 80);
+		}
+		Elements links = doc.select("a[href]");
+		Elements media = doc.select("[src]");
+		Elements imports = doc.select("link[href]");
+
+		print("\nLinks: (%d)", links.size());
+		for (Element link : links) {
+			print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+		}
+
+	}
+
+	/**
+	 * @param text
+	 * @param preferredWidth
+	 * @throws FileNotFoundException
+	 */
+	private static void debugLog(String text, int preferredWidth) throws FileNotFoundException {
+		PrintWriter sgxWriter = null;
+		try {
+			String[] allWords = text.split(" ");
+			StringBuilder sb = new StringBuilder();
+			sgxWriter = new PrintWriter("sgx.txt");
+			int currentLength = 0;
+			for (int i = 0; i < allWords.length; i++) {
+				if (currentLength + allWords[i].length() < preferredWidth) {
+					sb.append(allWords[i]);
+					sb.append(" ");
+					currentLength += allWords[i].length() + 1;
+				} else {
+					sgxWriter.println(sb.toString());
+					sb.delete(0, sb.length());
+					currentLength = 0;
+				}
+			}
+		} finally {
+			if (sgxWriter != null) {
+				sgxWriter.close();
+			}
+		}
+
+	}
+
+	private static void print(String msg, Object... args) {
+		System.out.println(String.format(msg, args));
+	}
+
+	private static String trim(String s, int width) {
+		if (s.length() > width)
+			return s.substring(0, width - 1) + ".";
+		else
+			return s;
+	}
+}
 
 /*
  * https://www.fool.sg/company/United+Overseas+Bank+Ltd/?ticker=SGX-U11
