ffc08676862e631c5089c74dbb2928b18b1ae673
Debmalya Jash <debmalya.jash@gmail.com>
2016-08-02 07:25:56 +0800

    Added ShareJunction

---
+++ src/main/java/scrapper/sgx/ShareJunctionScrapper.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package scrapper.sgx;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class ShareJunctionScrapper {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		parse();
+
+	}
+	
+	public static void parse() throws IOException{
+		String url = "http://www.sharejunction.com/sharejunction/viewStock.htm?id=24453";
+		Element doc = Jsoup.connect(url).get();
+		Element stockInfo = doc.getElementById("stockinfo");
+		System.out.println(stockInfo.text());
+	}
+
+}
